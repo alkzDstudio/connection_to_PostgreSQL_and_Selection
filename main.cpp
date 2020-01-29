fd0a63ab512b225d006983d8d7801aa8e9f5d387
alkzDstudio <34172722+alkzDstudio@users.noreply.github.com>
2020-01-30 01:36:02 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,16 +3,15 @@
 #include "SelectDataToSQL.h"
 
 int main() {
-  std::string db 			  = "LanitTercom";
+  std::string db 	= "LanitTercom";
   std::string dbserver	= "127.0.0.1";
   std::string uname    	= "LanitTercom";
   std::string pass     	= "1111";
   std::string table     = "playground";
 
-	SelectDataToSQL con (db, dbserver, uname, pass);
+  SelectDataToSQL con (db, dbserver, uname, pass);
 
-	std::cout << con.selectData(table)[1][2] << std::endl;
+  std::cout << con.selectData(table)[1][2] << std::endl;
 
-
-	return 0;
+  return 0;
 }
