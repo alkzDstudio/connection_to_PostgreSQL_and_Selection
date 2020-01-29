3b9b1c423b97bcda75d4e916cf84d296a38f8576
alkzDstudio <34172722+alkzDstudio@users.noreply.github.com>
2020-01-30 01:35:22 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <cstring>
-#include "SelectDataFromSQL.h"
+#include "SelectDataToSQL.h"
 
 int main() {
   std::string db 			  = "LanitTercom";
@@ -9,7 +9,7 @@ int main() {
   std::string pass     	= "1111";
   std::string table     = "playground";
 
-	SelectDataFromSQL con (db, dbserver, uname, pass);
+	SelectDataToSQL con (db, dbserver, uname, pass);
 
 	std::cout << con.selectData(table)[1][2] << std::endl;
 
