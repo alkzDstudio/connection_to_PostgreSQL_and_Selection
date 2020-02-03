211a5a0c9a6024012efe30d93f0e8a4c60dfd7cc
alkzDstudio <34172722+alkzDstudio@users.noreply.github.com>
2020-02-03 17:48:04 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,15 @@
+// A sketch of connecting to PostgreSQL
+
 #include <iostream>
 #include <cstring>
 #include "SelectDataToSQL.h"
 
 int main() {
-  std::string db 	= "LanitTercom";
+  std::string db 	      = "DBname";
   std::string dbserver	= "127.0.0.1";
-  std::string uname    	= "LanitTercom";
+  std::string uname    	= "userName";
   std::string pass     	= "1111";
-  std::string table     = "playground";
+  std::string table     = "tableName";
 
   SelectDataToSQL con (db, dbserver, uname, pass);
 
